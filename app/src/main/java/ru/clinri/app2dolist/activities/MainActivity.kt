855ca9c248a26537380d58e4abfae67de4026213
Andrei Borisov <clinri@ya.ru>
2021-10-31 21:11:12 +0300

    Lesson 6.  https://www.youtube.com/watch?v=b8MZM81_ZkA

--- app/src/main/java/ru/clinri/app2dolist/activities/MainActivity.kt
+++ app/src/main/java/ru/clinri/app2dolist/activities/MainActivity.kt
@@ -2,11 +2,37 @@ package ru.clinri.app2dolist.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import ru.clinri.app2dolist.R
+import ru.clinri.app2dolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomNavListener()
+    }
+
+    private fun setBottomNavListener(){
+        binding.bNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.settings ->{
+                    Log.d("MyLog","Settings")
+                }
+                R.id.notes->{
+                    Log.d("MyLog","Notes")
+                }
+                R.id.to_do_list ->{
+                    Log.d("MyLog","List")
+                }
+                R.id.new_item ->{
+                    Log.d("MyLog","New")
+                }
+            }
+            true
+        }
     }
 }
