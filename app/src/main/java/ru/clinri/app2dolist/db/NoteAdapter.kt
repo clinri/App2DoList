0fbae053cbed7e60eff06e1545e8ed82024759bb
Andrei Borisov <clinri@ya.ru>
2021-11-01 22:30:02 +0300

    Lesson 8.  https://www.youtube.com/watch?v=dyPxXpd_sNM

---
+++ app/src/main/java/ru/clinri/app2dolist/db/NoteAdapter.kt
@@ -0,0 +1,53 @@
+package ru.clinri.app2dolist.db
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.clinri.app2dolist.R
+import ru.clinri.app2dolist.databinding.NoteListItemBinding
+import ru.clinri.app2dolist.entities.NoteItem
+
+class NoteAdapter : ListAdapter<NoteItem, NoteAdapter.ItemHolder>(ItemComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        return ItemHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        holder.setData(getItem(position))
+    }
+
+    class ItemHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = NoteListItemBinding.bind(view)
+        fun setData(note: NoteItem) = with(binding) {
+            tvTitle.text = note.title
+            tvDiscription.text = note.content
+            tvTitle.text = note.time
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): ItemHolder {
+                return ItemHolder(
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.note_list_item, parent, false)
+                )
+            }
+        }
+    }
+
+    class ItemComparator: DiffUtil.ItemCallback<NoteItem>() {
+        override fun areItemsTheSame(oldItem: NoteItem, newItem: NoteItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: NoteItem, newItem: NoteItem): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+
+}
