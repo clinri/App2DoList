18adb08b8d17c0fe34bb8e65868afe3a1ea46dbb
Andrei Borisov <clinri@ya.ru>
2021-11-21 15:27:45 +0300

    add dialog New List. Lesson 22. https://youtu.be/jYMwyG902OA

--- app/src/main/java/ru/clinri/app2dolist/activities/MainActivity.kt
+++ app/src/main/java/ru/clinri/app2dolist/activities/MainActivity.kt
@@ -5,10 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import ru.clinri.app2dolist.R
 import ru.clinri.app2dolist.databinding.ActivityMainBinding
+import ru.clinri.app2dolist.dialogs.NewListDialog
 import ru.clinri.app2dolist.fragments.FragmentManager
 import ru.clinri.app2dolist.fragments.NoteFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NewListDialog.Listener {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +33,16 @@ class MainActivity : AppCompatActivity() {
                     Log.d("MyLog","List")
                 }
                 R.id.new_item ->{
-                    FragmentManager.currentFrag?.onClickNew()
+                    //FragmentManager.currentFrag?.onClickNew()
+                    NewListDialog.showDialog(this,this)
                     Log.d("MyLog","New")
                 }
             }
             true
         }
+    }
+
+    override fun onClick(name: String) {
+        Log.d("MyLog","Name: $name")
     }
 }
